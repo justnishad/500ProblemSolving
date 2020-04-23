f2b93f7196864dbc39cec9ca5a328851a9c8022c
justnishad <44561685+justnishad@users.noreply.github.com>
2020-04-24 00:56:54 +0530

    Create CheckSubArrayWithSumZero.py

---
+++ #Problem-Statement:https:/www.techiedelight.com/check-subarray-with-0-sum-exists-not/CheckSubArrayWithSumZero.py
@@ -0,0 +1,25 @@
+#Problem-Statement:https://www.techiedelight.com/check-subarray-with-0-sum-exists-not/
+arr = input()
+arr = arr.split()
+sums = 0
+set = []
+x = 0
+set.append(sums)
+for i in range(len(arr)):
+    sums = sums + int(arr[i])
+    if (sums in set):
+        x = 1
+        break
+    else:
+        set.append(sums)
+
+if (x == 1):
+    print('e')
+else:
+    print('ne')
+'''
+Explaination:
+We create a set which stores sum of the elemnents at each iteration.
+First element of that set is 0.
+It is kept in order to find wether sum at any stage ever gets to zero or not.
+'''
